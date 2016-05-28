fab1a3c61286b6157b6fe0cc8a5f1f718cff5b31
Tom Morris <tom_morris@btinternet.com>
2016-05-28 17:49:02 +0100

    update made

---
+++ README.md
@@ -0,0 +1,1 @@
+gistapi is a PHP project aimed at showing how a restful API can be accessed to provide gist updates on github
---
+++ gistapi.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+    require("github-creds.php");
+    $data = json_encode([
+        'description' => 'Gist created by API',
+        'public' => 'true',
+        'files' => [
+        'text.txt' => [ 'content' => 'Some more riveting text' ]
+        ]
+    ]);
+
+    $url = "https://api.github.com/gists";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,
+    ['Content-Type: application/javascript',
+	'Authorization: token ' . $access_token,
+	'User-Agent: php-curl']
+    );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $gist = json_decode($result, true);
+    if($gist) {
+        echo "Your gist is located at " . $gist['html_url'];
+    }
+?>
