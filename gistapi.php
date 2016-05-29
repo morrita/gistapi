d9d0abd32f5af5d7725a55c2c39d125f98af0167
Tom Morris <tom_morris@btinternet.com>
2016-05-29 13:55:25 +0100

    update made

--- gistapi.php
+++ gistapi.php
@@ -1,23 +1,29 @@
 #!/usr/bin/php
+
 <?php
-    require("github-creds.php");
-    $data = json_encode([
-        'description' => 'Gist created by API',
-        'public' => 'true',
-        'files' => [
-        'text.txt' => [ 'content' => 'Some more riveting text' ]
-        ]
-    ]);
+    require("github-creds.php");  // get access token for github
+
+    $content_obj = new stdClass();
+    $content_obj->content = 'even more riveting text';
+
+    $files_obj = new stdClass();
+    $files_obj->{'text.txt'} = $content_obj;
+
+    $data_obj = new stdClass();
+    $data_obj->description = 'Gist OBJECT created by a restful API';
+    $data_obj->public = 'true';
+    $data_obj->files = $files_obj;
+    $json_data = json_encode($data_obj);
 
     $url = "https://api.github.com/gists";
     $ch = curl_init($url);
+
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
     curl_setopt($ch, CURLOPT_HTTPHEADER,
     ['Content-Type: application/javascript',
 	'Authorization: token ' . $access_token,
-	'User-Agent: php-curl']
-    );
+	'User-Agent: php-curl']);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $result = curl_exec($ch);
@@ -25,6 +31,6 @@
 
     $gist = json_decode($result, true);
     if($gist) {
-        echo "Your gist is located at " . $gist['html_url'];
+        echo "Your gist is located at: " . $gist['html_url'] . "\n";
     }
 ?>
